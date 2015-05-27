505ca127e5a0f6c31523e890b0bcdc43b55c0b1c
cholet852 <cholet25@gmail.com>
2015-05-27 16:53:12 +0200

    Création des Class "Carte et MoteurDeCarte"

--- JeuDeCarte/Program.cs
+++ JeuDeCarte/Program.cs
@@ -4,61 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace JeuDeCarte
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            string[] Cartes = new string[52];
 
-            string[] typesCartes = new string[4] {"coeur", "carreau","pique","trèfle"};
+            MoteurDeCartes moteur = new MoteurDeCartes();
 
-            int index = 0;
+            List<Carte> Cartes = moteur.RetournerNouveauPaquet();
 
-            for (int typesCarte = 0; typesCarte < 4; typesCarte++)
-            {
-                Cartes[index] = string.Format("As de {0}", typesCartes[typesCarte]);
-                index++;
-
-                for(int i = 2; i<11; i++)
-                {
-                    Cartes[index] = string.Format("{0} de {1}", i, typesCartes[typesCarte]);
-                    index++;
-                }
-                Cartes[index] = string.Format("valet de {0}", typesCartes[typesCarte]);
-                index++;
-
-                Cartes[index] = string.Format("dame de {0}", typesCartes[typesCarte]);
-                index++;
-
-                Cartes[index] = string.Format("roi de {0}", typesCartes[typesCarte]);
-                index++;
-            }
-
-            string[] CartesBrassees = new string[52];
-
-            Random rnd = new Random();
+           // List<Carte> CartesBrassees = moteur.BrasseCarte(Cartes);
+            Carte carte = new Carte();
 
             for (int i = 0; i < 52; i++)
             {
-                bool cartePlacee = false;
-                while(!cartePlacee)
-                {
-                    int j = rnd.Next(0, 52);
-                    if(CartesBrassees[j] == null)
-                    {
-                        CartesBrassees[j] = Cartes[i];
-                        cartePlacee = true;
-                    }
-                }
+                Console.WriteLine(string.Format("Valeur: {0}", Cartes[i].Valeur));
+                Console.WriteLine(string.Format("Type: {0}", carte.RetourneTypeCarteTexte(Cartes[i].TypeCarte)));
+                Console.WriteLine(string.Format("Valeur: {0}", Cartes[i].Nom));
+                Console.ReadLine();
             }
 
-                for (int i = 0; i < 52; i++)
-                {
-                    Console.WriteLine(CartesBrassees[i]);
-                    Console.ReadLine();
-                }
         }
     }
 }
